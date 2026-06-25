1851830bd95a815afea7cf3ff04a9c320fd9a32a
Alaa Nazih Rabie Helmy <Alaanazih87_sd2023@science.bsu.edu.eg>
2026-06-25 16:23:14 +0300

    "implement alert repository"

--- lib/data/repositories/alert_repo_impl.dart
+++ lib/data/repositories/alert_repo_impl.dart
@@ -1,3 +1,5 @@
+import 'package:caregiver_app/core/failures.dart';
+
 import '../../core/network_info.dart';
 import '../../logic/repositories/i_alert_repo.dart';
 import '../models/alert_model.dart';
@@ -10,27 +12,68 @@ class AlertRepositoryImpl implements IAlertRepository {
   AlertRepositoryImpl({required this.remote, required this.networkInfo});
 
   @override
-  Stream<List<AlertModel>> watchAlerts(String elderlyId) {
-    throw UnimplementedError();
+  @override
+  Stream<List<AlertModel>> watchAlerts(String elderlyId) async* {
+    if (!await networkInfo.isConnected) {
+      throw const NetworkFailure();
+    }
+
+    try {
+      yield* remote.watchAlerts(elderlyId);
+    } catch (e) {
+      throw ServerFailure(details: e.toString());
+    }
+  }
+
+ @override
+  Future<List<AlertModel>> getAlerts(String elderlyId) async {
+    if (!await networkInfo.isConnected) {
+      throw const NetworkFailure();
+    }
+
+    try {
+      return await remote.getAlerts(elderlyId);
+    } catch (e) {
+      throw ServerFailure(details: e.toString());
+    }
+  }
+
+ @override
+  Future<AlertModel> sendAlert(AlertModel alert) async {
+    if (!await networkInfo.isConnected) {
+      throw const NetworkFailure();
+    }
+
+    try {
+      return await remote.sendAlert(alert);
+    } catch (e) {
+      throw ServerFailure(details: e.toString());
+    }
+  }
+
+@override
+  Future<void> markAsRead(String alertId) async {
+    if (!await networkInfo.isConnected) {
+      throw const NetworkFailure();
+    }
+
+    try {
+      await remote.markAsRead(alertId);
+    } catch (e) {
+      throw ServerFailure(details: e.toString());
+    }
   }
 
   @override
-  Future<List<AlertModel>> getAlerts(String elderlyId) {
-    throw UnimplementedError();
-  }
+  Future<void> deleteAlert(String alertId) async {
+    if (!await networkInfo.isConnected) {
+      throw const NetworkFailure();
+    }
 
-  @override
-  Future<AlertModel> sendAlert(AlertModel alert) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> markAsRead(String alertId) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> deleteAlert(String alertId) {
-    throw UnimplementedError();
+    try {
+      await remote.deleteAlert(alertId);
+    } catch (e) {
+      throw ServerFailure(details: e.toString());
+    }
   }
 }
